3803bbf74834f20ee5590ff33df933e0392dc5a3
Denis Panteleev <delavaifag@gmail.com>
2024-05-22 20:25:12 +0300

    save progress

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,5 +1,6 @@
 let initBurger = function () {
   let page = document.querySelector('.page')
+  let pageContent = page.querySelector('.page__content')
   let burgerBtn = document.querySelector('#burger-btn')
   let burgerMenu = document.querySelector('.burger-menu')
   let burgerCloseBtn = document.querySelector('.burger-menu__close-btn')
@@ -8,10 +9,12 @@ let initBurger = function () {
     burgerMenu.classList.toggle('burger-menu--hide')
     burgerBtn.classList.add('burger-button--hide')
     page.classList.add('page--modal-on')
+    pageContent.classList.add('page__content--burger-show')
   })
   burgerCloseBtn.addEventListener('click', function () {
     burgerMenu.classList.toggle('burger-menu--hide')
     page.classList.remove('page--modal-on')
+    pageContent.classList.remove('page__content--burger-show')
   })
 
   function adjustAside() {
